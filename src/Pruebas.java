63d0650b2e8c13c0704663cd59f1059f7b3f7aef
Juanfran Benito <jbc0056@alu.ubu.es>
2018-02-22 12:35:23 +0100

    Copiado un proyecto de pruebas.

---
+++ src/Pruebas.java
@@ -0,0 +1,32 @@
+import java.io.InputStream;
+import java.net.URL;
+import javax.
+
+public class Pruebas {
+
+	public static void main(String[] args) {
+		URL url = new  URL("https://graph.facebook.com/search?q=java&type=post");
+		   try (InputStream is = url.openStream();
+		         JsonParser parser = Json.createParser(is)) {
+		        while (parser.hasNext()) {
+		            Event e = parser.next();
+		            if (e == Event.KEY_NAME) {
+		                switch (parser.getString()) {
+		                   case "name":
+		                        parser.next();
+		                       System.out.print(parser.getString());
+		                      System.out.print(":  ");
+		                      break;
+		                  case "message":
+		                      parser.next();
+		                      System.out.println(parser.getString());
+		                      System.out.println("---------");
+		                      break;
+		               }
+		           }
+		       }
+		   }
+
+	}
+
+}
